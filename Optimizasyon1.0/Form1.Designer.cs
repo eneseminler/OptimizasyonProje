409d441bd114a2300934b1b6a0a3ad5f36f78eff
eneseminler <36516539+eneseminler@users.noreply.github.com>
2018-11-07 20:25:49 +0300

    Bitti sayılır

---
+++ Optimizasyon1.0/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿using System;
+
+namespace Optimizasyon1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Satır sayısı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 7);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sütun sayısı";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 29);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(386, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ZMin: ";
+            // 
+            // button3
+            // 
+            this.button3.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Location = new System.Drawing.Point(558, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(19, 20);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(158, 10);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Satır değeri";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 32);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Sütun değeri";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(223, 10);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(222, 32);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(474, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Sa";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(387, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Vi =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(685, 314);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Optimizasyon1.0/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimizasyon1._0
+{
+    public partial class Form1 : Form
+    {
+        string strdeger, stndeger;
+        int str = 0, stn = 0;
+        int[,] dizi;
+        int[,] dizi2;
+        TextBox[,] TextKutu;
+        TextBox[] TextKutuBos;
+        TextBox[] TextKutuBos2;
+        TextBox[,] TextKutu2;
+        Label[,] LabelKutu;
+        Label[] LabelArz;
+        Label[] LabelTalep;
+        int[] U,V,O;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            
+        }
+        private void ZminHesap()
+        {
+            int toplam = 0;
+            for (int i = 0; i < str; i++)
+            {
+                for (int k = 0; k < stn; k++)
+                {
+                    if (dizi2[i, k] != -1)
+                    {
+                       
+                        toplam += dizi2[i, k] * dizi[i, k];
+                    }
+                    label3.Text = Convert.ToString("Zmin: " + toplam);
+                }
+            }
+            
+        }
+        private void KutuOlustur()
+        {
+            str = Convert.ToInt32(textBox1.Text);
+            stn = Convert.ToInt32(textBox2.Text);
+            strdeger = Convert.ToString(textBox3.Text);
+            stndeger = Convert.ToString(textBox4.Text);
+            //Dizileri oluşturuyor
+            TextKutu = new TextBox[str, stn];
+            TextKutuBos = new TextBox[str + stn];
+            TextKutuBos2 = new TextBox[str + stn];
+            TextKutu2 = new TextBox[str, stn];
+            LabelKutu = new Label[str, stn];
+            LabelArz = new Label[stn+1];
+            LabelTalep = new Label[str + 1];
+            
+            U = new int[str];
+            V = new int[stn];
+            O = new int[str * stn];
+
+            dizi = new int[str, stn];
+            dizi2 = new int[str, stn];
+            
+            
+            //Formdaki Textboxlar oluşturuluyor
+            int left = 100;
+            int top = 120;
+            int left2 = 155;
+            
+            for (int i = 0; i < str; i++)
+            {
+                for (int k = 0; k < stn; k++)
+                {
+                    TextKutu[i, k] = new TextBox();
+                    TextKutu[i, k].Left = left;
+                    TextKutu[i, k].Top = top;
+                    TextKutu[i, k].Width = 50;
+                    TextKutu[i, k].Text = "";
+                    this.Controls.Add(TextKutu[i, k]);
+                    
+                    TextKutu2[i, k] = new TextBox();
+                    TextKutu2[i, k].Left = left2;
+                    TextKutu2[i, k].Top = top;
+                    TextKutu2[i, k].Width = 20;
+                    TextKutu2[i, k].Text = "";
+                    this.Controls.Add(TextKutu2[i, k]);
+                    
+
+                    left += 90;
+                    left2 += 90;
+                }
+                //arz sütunu
+                TextKutuBos[i] = new TextBox();
+                TextKutuBos[i].Left = left;
+                TextKutuBos[i].Top = top;
+                TextKutuBos[i].Width = 50;
+                TextKutuBos[i].Text = "";
+                this.Controls.Add(TextKutuBos[i]);
+
+
+                top = top + 60;
+                left = 100;
+                left2 = 155;
+
+            }
+            
+            //talep satırı
+            for (int i = 0; i < stn+1 ; i++)
+            {
+
+                TextKutuBos2[i] = new TextBox();
+                TextKutuBos2[i].Left = left;
+                TextKutuBos2[i].Top = top;
+                TextKutuBos2[i].Width = 50;
+                TextKutuBos2[i].Text = "";
+                this.Controls.Add(TextKutuBos2[i]);
+             
+                left += 90;
+           
+                //label6.Text = Convert.ToString();
+            }
+            
+         
+            //Formdaki labellar oluşturuluyor
+            int labeltop = 120;
+            int labelleft = 20;
+            for (int i = 0; i < str; i++)
+            {
+                int k = 0;
+                LabelKutu[i, k] = new Label();
+                LabelKutu[i, k].Left =labelleft;
+                LabelKutu[i, k].Top = labeltop;
+                LabelKutu[i, k].Width = 60;
+                LabelKutu[i, k].Text = strdeger + (i+1) ;
+                this.Controls.Add(LabelKutu[i, k]);
+                
+                labeltop += 60;
+            }
+            //talep satırı
+            int tlp = 0;
+            LabelTalep[tlp] = new Label();
+            LabelTalep[tlp].Left = labelleft;
+            LabelTalep[tlp].Top = labeltop;
+            LabelTalep[tlp].Width = 60;
+            LabelTalep[tlp].Text = "Talep";
+            this.Controls.Add(LabelTalep[tlp]);
+            tlp++;
+            
+            labelleft = 100;
+            for (int k = 0; k < stn; k++)
+            {
+
+                int i = 0;
+                LabelKutu[i, k] = new Label();
+                LabelKutu[i, k].Left = labelleft;
+                LabelKutu[i, k].Top = 90;
+                LabelKutu[i, k].Width = 60;
+                LabelKutu[i, k].Text = stndeger + (k+1);
+                this.Controls.Add(LabelKutu[i, k]);
+
+                labelleft += 90;
+                
+            }
+            int arzf = 0;
+            LabelArz[arzf] = new Label();
+            LabelArz[arzf].Left = labelleft;
+            LabelArz[arzf].Top = 90;
+            LabelArz[arzf].Width = 60;
+            LabelArz[arzf].Text = "Arz";
+            this.Controls.Add(LabelArz[arzf]);
+            arzf++;
+
+            DiziDoldur(str, stn);
+
+        }
+
+        private void MatrisDoldur()
+        {
+            for (int i = 0; i < str; i++)
+            {
+                for (int k = 0; k < stn; k++)
+                {
+
+                    if(Convert.ToString(TextKutu[i, k].Text).Length != 0)
+                    {
+                        dizi[i, k] = Convert.ToInt32(TextKutu[i, k].Text);
+                    }
+                    else
+                    {
+                        var LabelHataEksik = new Label();
+                        LabelHataEksik.Left = 20;
+                        LabelHataEksik.Top = 60;
+                        LabelHataEksik.Width = 300;
+                        LabelHataEksik.Text = "Eksik matris değerleri girdiniz! Bu şekilde kontrol edemezsiniz.";
+                        this.Controls.Add(LabelHataEksik);
+
+                    }
+                    if(Convert.ToString(TextKutu2[i, k].Text).Length != 0)
+                    {
+                        dizi2[i, k] = Convert.ToInt32(TextKutu2[i, k].Text);
+                    }
+                }           
+            }
+            // TextKutu[1, 1].Text = Convert.ToString(dizi[0, 0] + dizi2[1, 1]);
+            for (int i = 0; i < str; i++)
+            {
+                U[i] = -25;
+            }
+            for (int i = 0; i < stn; i++)
+            {
+                V[i] = -25;
+            }
+            MatrisHesapla();
+            OptimumHesapla();
+            
+          
+        
+        }
+        private void DiziDoldur(int a, int b)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int k = 0; k < b; k++)
+                {
+                    dizi[i, k] = -1;
+                    dizi2[i, k] = -1;
+                }
+            }
+
+        }
+        private void MatrisHesapla()
+        {
+            //Buraya geldi1
+            U[0] = 0;
+            
+            for (int p = 0; p < 5; p++)
+            {
+                for (int i = 0; i < str; i++)
+                {
+                    for (int k = 0; k < stn; k++)
+                    {
+                        if (dizi2[i,k] != -1)
+                        {
+                           
+                            if (-25 != U[i]) {
+                                V[k] = dizi[i, k] - U[i];
+                             //   TextKutu[i,k].Text = Convert.ToString(V[k] + "+" + k );
+                            }
+                            if (-25 != V[k])
+                            {
+                                U[i] = dizi[i, k] - V[k];
+                              // TextKutu[i, k].Text = Convert.ToString(U[i] + "-" + i);
+                            }
+                        }
+                    }
+                }
+               
+            }
+            ArzTopla();
+            //u0 u 1 v1 v3
+            //LabelKutu[0, 0].Text = Convert.ToString(U[0]);
+            //LabelKutu[1, 0].Text = Convert.ToString(U[1]);
+            //LabelKutu[2, 0].Text = Convert.ToString(U[2]);
+            //LabelKutu[3, 0].Text = Convert.ToString(U[3]);
+            //LabelKutu[0, 0].Text = Convert.ToString(V[0]);
+            //LabelKutu[0, 1].Text = Convert.ToString(V[1]);
+            //LabelKutu[0, 2].Text = Convert.ToString(V[2]);
+            //LabelKutu[0, 3].Text = Convert.ToString(V[3]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult tus;
+            tus = colorDialog1.ShowDialog();
+            if (tus == DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+      private void ArzTopla()
+        {
+            int toplam = 0;
+            int toplam2 = 0;
+            for (int i = 0; i < str; i++)
+            {
+                toplam += Convert.ToInt32(TextKutuBos[i].Text);
+            }
+            for (int i = 0; i < stn; i++)
+            {
+                toplam2 += Convert.ToInt32(TextKutuBos2[i].Text);
+            }
+            if (toplam != toplam2)
+            {
+                label6.Text = "Arz ve talep eşit değil.";
+                label6.Visible = true;
+            }
+            else
+            {
+                label6.Visible = false;
+            }
+            TextKutuBos2[stn].Text = Convert.ToString(toplam + " | " + toplam2);
+        }
+
+        private void OptimumHesapla()
+        {
+            int a = 0;
+            for (int i = 0; i < str; i++)
+            {
+                for (int k = 0; k < stn; k++)
+                {
+                    if (dizi2[i, k] == -1)
+                    {
+                        O[a] = dizi[i, k] - (U[i] + V[k]);
+                        label7.Text = label7.Text + " - V" + (a+1) + "=" + O[a];
+                        //TextKutu[i, k].Text = Convert.ToString(O[a]);
+                        a++;
+                    }
+                }
+            }
+            ZminHesap();
+            for (int i = 0; i < a; i++)
+            {
+            //    TextKutu[0, 0].Text = Convert.ToString(O[0]);
+            //    TextKutu[0, 1].Text = Convert.ToString(O[1]);
+            //    TextKutu[1, 0].Text = Convert.ToString(O[2]);
+            //    TextKutu[1, 1].Text = Convert.ToString(O[3]);
+                if (O[i] < 0)
+                {
+                    var LabelHata = new Label();
+                    LabelHata.Left = 20;
+                    LabelHata.Top = 60;
+                    LabelHata.Width = 500;
+                    LabelHata.Text = "Bu çözüm optimum yol değildir. En iyi sonuç bulunamadı. Negatif değer var.";
+                    this.Controls.Add(LabelHata);
+                    OptimumDegil();
+                    break;
+                }
+
+            }
+            var LabelDogru = new Label();
+            LabelDogru.Left = 20;
+            LabelDogru.Top = 60;
+            LabelDogru.Width = 500;
+            LabelDogru.Text = "Bu çözüm optimum yoldur. En iyi sonuç bulundu. Tüm değerler pozitif ya da 0.";
+            this.Controls.Add(LabelDogru);
+            
+        }
+        private void OptimumDegil()
+        {
+            for (int i = 0; i < str; i++)
+            {
+                for (int k = 0; k < stn; k++)
+                {
+                    TextKutu[i, k].Text = "";
+                    TextKutu2[i, k].Text = "";
+                }
+            }
+            label6.Text = "";
+            DiziDoldur(str,stn);
+            TextKutuBos2[stn].Text = "";
+        }           
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KutuOlustur();
+            button1.Hide();
+            button2.Show();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MatrisDoldur();
+      
+
+        }
+    }
+}
